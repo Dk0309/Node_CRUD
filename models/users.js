04405fdbbd6df812909bc59b33425c3a6478202c
Deepak Kumar <70138380+Dk0309@users.noreply.github.com>
2024-12-30 18:41:02 +0530

    Add files via upload

---
+++ models/users.js
@@ -0,0 +1,27 @@
+const { type } = require('express/lib/response');
+const mongoose = require('mongoose');
+const userSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        require: true,
+    },
+    email: {
+        type: String,
+        require: true,
+    },
+    phone: {
+        type: String,
+        require: true,
+    },
+    image: {
+        type: String,
+        require: true,
+    },
+    created: {
+        type: Date,
+        require: true,
+        default: Date.now,
+    }
+});
+
+module.exports = mongoose.model('User', userSchema);
